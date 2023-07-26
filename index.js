bdce884b14cc4862520d70623612df8ad29ae7f4
chaya <chayavogel1@gmail.com>
2023-07-26 15:42:12 -0400

    small change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", " ");
     const left = parseInt(leftNumbers, 10);
-    if (left < 380) {
+    if (left < 360) {
         dodger.style.left = `${left+1}px`
     }
 }
